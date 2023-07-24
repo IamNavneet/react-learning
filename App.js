3e50b1a7733300b294c81d272d589a199b7ec8b7
Navneet Pandey <13730@teamcomputers.com.com>
2023-07-24 11:02:58 +0530

    parcel installed and intro completed

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-const heading = React.createElement("div", {id:"heading", className:"colors"}, [
-    React.createElement("div", {}, [React.createElement("h1", {}, "div 1 h1 tag"), React.createElement("h2", {}, "div 1 h2 tag")]),
-    React.createElement("div", {}, [React.createElement("h1", {}, "div 2 h1 tag"), React.createElement("h2", {}, "div 1 h2 tag")])
-])
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("div", {id:"heading", className:"colors"}, [
+//     React.createElement("div", {}, [React.createElement("h1", {}, "div 1 h1 tag"), React.createElement("h2", {}, "div 1 h2 tag")]),
+//     React.createElement("div", {}, [React.createElement("h1", {}, "div 2 h1 tag"), React.createElement("h2", {}, "div 1 h2 tag")])
+// ])
+
+const jsxHeading = <h1 id="heading">This is Jsx code</h1>;
+
+const JsxHeadingComponent = () => {
+    return <h1>this is jsx heading</h1>
+}
+
+const title = (
+    <h2 className="colors"> this is title and it is a react element</h2>
+)
+
+const HeadingComponent = () =>{
+    return (
+        <>
+        {title}
+        <h1>This is composition component </h1>
+        <JsxHeadingComponent />
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
+root.render(<HeadingComponent />);
+
